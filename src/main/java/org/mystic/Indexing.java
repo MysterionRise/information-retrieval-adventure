55f422dd8ea4f07a1c0ddf6ea688aca0dc2272e3
Mysterion <pericov.ko@gmail.com>
2013-09-02 00:22:49 +0400

    Fix multivalued fields + add some search via Lucene API

--- src/main/java/org/mystic/Indexing.java
+++ src/main/java/org/mystic/Indexing.java
@@ -14,19 +14,26 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class OrdTest {
+/**
+ * @author kperikov
+ *         <p/>
+ *         <p/>
+ *         Connects to localhost Solr, and then index small part of russian wikipedia titles
+ */
+public class Indexing {
 
     // could remove 4, to get the biggest file to analyze
-    public static final String XML_FILE_PATH = "http://dumps.wikimedia.org/ruwiki/latest/ruwiki-latest-abstract4.xml";
+    private static final String XML_FILE_PATH = "http://dumps.wikimedia.org/ruwiki/latest/ruwiki-latest-abstract4.xml";
     private static final int MAX_SIZE = 1000;
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         SolrServer server = new HttpSolrServer("http://localhost:8983/solr/");
         try {
             server.deleteByQuery("*:*");
             XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
             XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(new URL(XML_FILE_PATH).openStream());
-            Collection<SolrInputDocument> docs = new ArrayList<SolrInputDocument>();
+            Collection<SolrInputDocument> docs = new ArrayList<>();
             SolrInputDocument doc = null;
             boolean isDocs = false;
             long id = 1;
@@ -43,7 +50,7 @@ public class OrdTest {
                         doc = new SolrInputDocument();
                     }
 
-                } else if (event.isCharacters() && isDocs) {
+                } else if (event.isCharacters() && isDocs && !event.asCharacters().getData().trim().isEmpty()) {
                     if ("title".equalsIgnoreCase(fieldName) || "url".equalsIgnoreCase(fieldName)) {
                         doc.addField(fieldName, event.asCharacters().getData());
                     }
@@ -65,6 +72,8 @@ public class OrdTest {
             System.out.println("-------------------Adding last chunk of docs to Solr-----------------");
             server.add(docs);
             server.commit();
+            server.shutdown();
+            System.out.println("Indexing of " + id + " documents, takes " + (System.currentTimeMillis() - start) / 1000 + " seconds");
         } catch (SolrServerException e) {
             System.out.println("Error in Solr" + e.getRootCause());
         } catch (IOException e) {
