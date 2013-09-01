68e713efed838e178a4e9eed03e52b23f2680328
Mysterion <pericov.ko@gmail.com>
2013-09-01 20:28:35 +0400

    Indexing works

--- src/main/java/org/mystic/OrdTest.java
+++ src/main/java/org/mystic/OrdTest.java
@@ -9,27 +9,29 @@ import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.XMLEvent;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class OrdTest {
 
-    public static final String XML_FILE_PATH = "/home/mysterion/Downloads/ruwiki-latest-abstract4.xml";
+    // could remove 4, to get the biggest file to analyze
+    public static final String XML_FILE_PATH = "http://dumps.wikimedia.org/ruwiki/latest/ruwiki-latest-abstract4.xml";
+    private static final int MAX_SIZE = 1000;
 
     public static void main(String[] args) {
         SolrServer server = new HttpSolrServer("http://localhost:8983/solr/");
         try {
             server.deleteByQuery("*:*");
             XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-            XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(
-                    new FileInputStream(new File(XML_FILE_PATH)));
+            XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(new URL(XML_FILE_PATH).openStream());
             Collection<SolrInputDocument> docs = new ArrayList<SolrInputDocument>();
             SolrInputDocument doc = null;
             boolean isDocs = false;
+            long id = 1;
             String fieldName = "";
+            System.out.println("-------------------Reading and parsing file " + XML_FILE_PATH + "-----------------");
             while (xmlEventReader.hasNext()) {
                 XMLEvent event = xmlEventReader.peek();
                 if (event.isStartElement()) {
@@ -39,7 +41,7 @@ public class OrdTest {
                     if ("doc".equalsIgnoreCase(event.asStartElement().getName().getLocalPart())) {
                         isDocs = true;
                         doc = new SolrInputDocument();
-                    }                                             doc != null &&
+                    }
 
                 } else if (event.isCharacters() && isDocs) {
                     if ("title".equalsIgnoreCase(fieldName) || "url".equalsIgnoreCase(fieldName)) {
@@ -48,12 +50,21 @@ public class OrdTest {
 
                 } else if (event.isEndElement()) {
                     if ("doc".equalsIgnoreCase(event.asEndElement().getName().getLocalPart())) {
+                        doc.addField("id", id++);
                         docs.add(doc);
                     }
                 }
                 xmlEventReader.nextEvent();
+                if (docs.size() > MAX_SIZE) {
+                    System.out.println("---------- Adding " + MAX_SIZE + " documents to Solr---------------");
+                    server.add(docs);
+                    server.commit();
+                    docs.clear();
+                }
             }
-            System.out.println(docs.size());
+            System.out.println("-------------------Adding last chunk of docs to Solr-----------------");
+            server.add(docs);
+            server.commit();
         } catch (SolrServerException e) {
             System.out.println("Error in Solr" + e.getRootCause());
         } catch (IOException e) {
