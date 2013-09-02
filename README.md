0a21b1db582d72d487dd9d09505a148cbefa9273
Konstantin Perikov <konstantin.perikov@kohls.com>
2013-09-02 11:19:10 +0400

    Convert everything to Scala

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 octo-solr-adventure
 ===================
 
-Contains some plays with Solr, Lucene
-First plan - to figure out work with ord and docvalues
+Contains some plays with Solr, Lucene using Scala
+* Indexing some small part of Russian wikipedia pages (only titles and URIs)
+* Figure out work with ord and docvalues
---
+++ src/main/scala/org/mystic/Searching.scala
@@ -0,0 +1,34 @@
+package org.mystic
+
+import scala.Predef.String
+import org.apache.lucene.store.FSDirectory
+import java.io.{IOException, File}
+import org.apache.lucene.index.{DirectoryReader, IndexReader}
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.document.Document
+import scala.util.Random
+
+/**
+ * @author kperikov
+ */
+object Searching {
+  private final val SOLR_HOME: String = "/home/kperikov/Downloads/solr-4.4.0"
+  private final val PATH_TO_INDEX: String = SOLR_HOME + "/example/solr/collection1/data/index"
+
+  def main(args: Array[String]) {
+    try {
+      val fsDirectory: FSDirectory = FSDirectory.open(new File(PATH_TO_INDEX))
+      val indexReader: IndexReader = DirectoryReader.open(fsDirectory)
+      val random: Random = new Random()
+      val searcher: IndexSearcher = new IndexSearcher(indexReader)
+      System.out.println(indexReader.numDocs)
+      val childDocument: Document = indexReader.document(random.nextInt(indexReader.numDocs()))
+
+    }
+    catch {
+      case e: IOException => {
+        System.out.println("Error while opening index" + e.getCause)
+      }
+    }
+  }
+}
