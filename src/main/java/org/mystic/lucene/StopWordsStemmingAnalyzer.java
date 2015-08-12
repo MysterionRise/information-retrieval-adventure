609347f57df04910bfcd7a41373379e6bd9f79b1
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2015-08-12 11:28:59 +0200

    Create StopWordsStemmingAnalyzer.java

---
+++ src/main/java/org/mystic/lucene/StopWordsStemmingAnalyzer.java
@@ -0,0 +1,39 @@
+package org.mystic.lucene;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.analysis.shingle.ShingleFilter;
+import org.apache.lucene.analysis.standard.StandardFilter;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.util.StopwordAnalyzerBase;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class StopWordsStemmingAnalyzer {
+
+    public static void main(String[] args) throws IOException {
+
+        String theSentence = "this is a mouse more mouses and thing things thoughts think thinks thinked";
+        StringReader reader = new StringReader(theSentence);
+        Tokenizer whitespaceTokenizer = new WhitespaceTokenizer();
+        whitespaceTokenizer.setReader(reader);
+        TokenStream tokenStream = new StopFilter(whitespaceTokenizer, StopAnalyzer.ENGLISH_STOP_WORDS_SET);
+        tokenStream = new PorterStemFilter(tokenStream);
+
+        final CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+        tokenStream.reset();
+
+        while (tokenStream.incrementToken()) {
+            System.out.println(charTermAttribute.toString());
+        }
+
+        tokenStream.end();
+        tokenStream.close();
+    }
+}
