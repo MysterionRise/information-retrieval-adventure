34633cd8b14dd469e1f16896a91ab2c3c5b7df0b
Konstantin Perikov <kperikov@griddynamics.com>
2015-10-23 23:50:18 +0200

    add weight impl

--- src/main/java/org/mystic/lucene/BM25FQuery.java
+++ src/main/java/org/mystic/lucene/BM25FQuery.java
@@ -1,16 +1,54 @@
 package org.mystic.lucene;
 
-import org.apache.lucene.search.DisjunctionMaxQuery;
-import org.apache.lucene.search.Query;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.search.*;
+import org.apache.lucene.util.Bits;
 
+import java.io.IOException;
 import java.util.Collection;
 
 /**
  * my implementation of BM25F approach for Lucene
  */
 public class BM25FQuery extends DisjunctionMaxQuery {
-    
+
     public BM25FQuery(Collection<Query> disjuncts, float tieBreakerMultiplier) {
         super(disjuncts, tieBreakerMultiplier);
+    }
+
+    @Override
+    public Weight createWeight(IndexSearcher searcher) throws IOException {
+        return new BM25FWeight(searcher);
+    }
+
+    private class BM25FWeight extends Weight {
+        
+        public BM25FWeight(IndexSearcher searcher) {
+        }
+
+        @Override
+        public Explanation explain(LeafReaderContext leafReaderContext, int i) throws IOException {
+            return null;
+        }
+
+        @Override
+        public Query getQuery() {
+            return null;
+        }
+
+        @Override
+        public float getValueForNormalization() throws IOException {
+            return 0;
+        }
+
+        @Override
+        public void normalize(float v, float v1) {
+
+        }
+
+        @Override
+        public Scorer scorer(LeafReaderContext leafReaderContext, Bits bits) throws IOException {
+            return null;
+        }
     }
 }
