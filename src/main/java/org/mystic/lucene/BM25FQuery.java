c12902de018df951166b6619fa33b204191551fd
Konstantin Perikov <kperikov@griddynamics.com>
2015-10-25 11:44:14 +0100

    add more things related to bm25f

--- src/main/java/org/mystic/lucene/BM25FQuery.java
+++ src/main/java/org/mystic/lucene/BM25FQuery.java
@@ -5,7 +5,9 @@ import org.apache.lucene.search.*;
 import org.apache.lucene.util.Bits;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * my implementation of BM25F approach for Lucene
@@ -21,34 +23,54 @@ public class BM25FQuery extends DisjunctionMaxQuery {
         return new BM25FWeight(searcher);
     }
 
-    private class BM25FWeight extends Weight {
-        
-        public BM25FWeight(IndexSearcher searcher) {
+    class BM25FWeight extends DisjunctionMaxWeight {
+
+
+        public BM25FWeight(IndexSearcher searcher) throws IOException {
+            super(searcher);
         }
 
         @Override
-        public Explanation explain(LeafReaderContext leafReaderContext, int i) throws IOException {
-            return null;
+        public Scorer scorer(LeafReaderContext context, Bits acceptDocs) throws IOException {
+            List<Scorer> scorers = new ArrayList<>();
+            for (Weight w : weights) {
+                // we will advance() subscorers
+                Scorer subScorer = w.scorer(context, acceptDocs);
+                if (subScorer != null) {
+                    scorers.add(subScorer);
+                }
+            }
+            if (scorers.isEmpty()) {
+                // no sub-scorers had any documents
+                return null;
+            } else if (scorers.size() == 1) {
+                // only one sub-scorer in this segment
+                return scorers.get(0);
+            } else {
+                return new BM25FScorer(this, scorers.toArray(new Scorer[scorers.size()]));
+            }
+        }
+    }
+
+    class BM25FScorer extends DisjunctionScorer {
+
+        protected BM25FScorer(Weight weight, Scorer[] subScorers) {
+            super(weight, subScorers);
         }
 
         @Override
-        public Query getQuery() {
-            return null;
+        protected void reset() {
+
         }
 
         @Override
-        public float getValueForNormalization() throws IOException {
+        protected void accum(Scorer subScorer) throws IOException {
+
+        }
+
+        @Override
+        protected float getFinal() {
             return 0;
-        }
-
-        @Override
-        public void normalize(float v, float v1) {
-
-        }
-
-        @Override
-        public Scorer scorer(LeafReaderContext leafReaderContext, Bits bits) throws IOException {
-            return null;
         }
     }
 }
