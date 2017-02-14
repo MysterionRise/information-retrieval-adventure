c808c7c0322b8ec05da62c0b6591b5f567bd2bf7
Konstantin Perikov <pericov.ko@gmail.com>
2017-02-14 14:11:21 +0300

    commit comma tokenizer usage

---
+++ lucene6/src/main/java/CommaTokenizer.java
@@ -0,0 +1,62 @@
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.pattern.PatternTokenizer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.BytesRef;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
+
+public class CommaTokenizer {
+
+    public static void main(String[] args) throws IOException {
+
+        Directory dir = new RAMDirectory();
+        Analyzer analyzer = new Analyzer() {
+            @Override
+            protected TokenStreamComponents createComponents(String fieldName) {
+                Tokenizer source = new PatternTokenizer(Pattern.compile("\\,"), -1);
+                return new TokenStreamComponents(source);
+            }
+        };
+        IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+        iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        IndexWriter writer = new IndexWriter(dir, iwc);
+
+        Document doc = new Document();
+        doc.add(new TextField("text", "Age 6, Age 7, Age 8", Field.Store.YES));
+        writer.addDocument(doc);
+        writer.close();
+
+        IndexReader reader = DirectoryReader.open(dir);
+        final Fields fields = MultiFields.getFields(reader);
+        final Iterator<String> iterator = fields.iterator();
+
+        while(iterator.hasNext()) {
+            final String field = iterator.next();
+            final Terms terms = MultiFields.getTerms(reader, field);
+            final TermsEnum it = terms.iterator();
+            BytesRef term = it.next();
+            while (term != null) {
+                System.out.println(term.utf8ToString());
+                term = it.next();
+            }
+        }
+
+
+    }
+}
