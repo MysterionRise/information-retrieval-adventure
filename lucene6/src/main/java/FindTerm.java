34b546c3462b3716d79928e33f3724dfa7637565
Konstantin Perikov <pericov.ko@gmail.com>
2017-05-04 21:58:51 +0300

    test byterefs

--- lucene6/src/main/java/FindTerm.java
+++ lucene6/src/main/java/FindTerm.java
@@ -1,8 +1,6 @@
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.*;
 import org.apache.lucene.index.*;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.MatchAllDocsQuery;
@@ -11,6 +9,7 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.BytesRef;
+import org.apache.lucene.util.NumericUtils;
 
 import java.io.IOException;
 import java.util.Comparator;
@@ -30,12 +29,15 @@ public class FindTerm {
 
         Document doc = new Document();
         doc.add(new TextField("text", "first second third", Field.Store.YES));
+        doc.add(new LegacyIntField("long", 1, Field.Store.YES));
         writer.addDocument(doc);
         doc = new Document();
         doc.add(new TextField("text", "second third", Field.Store.YES));
+        doc.add(new LegacyIntField("long", 2, Field.Store.YES));
         writer.addDocument(doc);
         doc = new Document();
         doc.add(new TextField("text", "third", Field.Store.YES));
+        doc.add(new LegacyIntField("long", 1, Field.Store.YES));
         writer.addDocument(doc);
         writer.close();
 
@@ -45,12 +47,13 @@ public class FindTerm {
 
         long maxFreq = Long.MIN_VALUE;
         String freqTerm = "";
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             final String field = iterator.next();
             final Terms terms = MultiFields.getTerms(reader, field);
             final TermsEnum it = terms.iterator();
             BytesRef term = it.next();
             while (term != null) {
+                System.out.println(term.utf8ToString() + " " + NumericUtils.sortableBytesToInt(term.bytes, term.offset));
                 final long freq = it.totalTermFreq();
                 if (freq > maxFreq) {
                     maxFreq = freq;
