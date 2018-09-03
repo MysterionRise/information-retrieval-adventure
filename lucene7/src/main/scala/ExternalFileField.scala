ecaf626bedf02335947e02efa6ea2675ae06f82f
Konstantin Perikov <konstantin.perikov@gmail.com>
2018-09-03 17:00:29 +0100

    update stuff

--- lucene7/src/main/scala/ExternalFileField.scala
+++ lucene7/src/main/scala/ExternalFileField.scala
@@ -1,3 +1,6 @@
+import java.io.{BufferedOutputStream, FileOutputStream, PrintWriter}
+import java.util
+
 import org.apache.solr.client.solrj.SolrClient
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer
 import org.apache.solr.common.SolrInputDocument
@@ -5,6 +8,7 @@ import org.apache.solr.common.params.ModifiableSolrParams
 import org.apache.solr.core.CoreContainer
 
 import scala.Console._
+import scala.util.Random
 
 /**
   * using external file field to filter stuff out
@@ -12,8 +16,18 @@ import scala.Console._
 object ExternalFileField {
 
   var server: SolrClient = null
+  val maxProd = 80000
+  val rand = new Random()
 
   def main(a: Array[String]) {
+    //    val out = new PrintWriter(new BufferedOutputStream(new FileOutputStream("account.txt")))
+    //    for (i <- 1 to maxProd) {
+    //      if (rand.nextInt(3) < 1) {
+    //        out.println("prod" + i + "=1")
+    //      }
+    //    }
+    //    out.flush()
+    //    out.close()
 
     try {
       val solrDir = ExternalFileField.getClass.getResource("/solr").getPath
@@ -21,32 +35,25 @@ object ExternalFileField {
       container.load()
       server = new EmbeddedSolrServer(container, "external-field")
 
-      val doc1 = new SolrInputDocument()
-      doc1.addField("id", "prod1")
-      doc1.addField("brand", "aaa")
-      server.add(doc1)
-
-      val doc2 = new SolrInputDocument()
-      doc2.addField("id", "prod2")
-      doc2.addField("brand", "aaa")
-
-      server.add(doc2)
-
-      val doc3 = new SolrInputDocument()
-      doc3.addField("id", "prod3")
-      doc3.addField("brand", "aaa")
-
-      server.add(doc3)
-
-      val doc4 = new SolrInputDocument()
-      doc4.addField("id", "prod4")
-      doc4.addField("brand", "aaa")
-      server.add(doc4)
-
-      val doc5 = new SolrInputDocument()
-      doc5.addField("id", "prod5")
-      doc5.addField("brand", "aaa")
-      server.add(doc5)
+      for (i <- 1 to maxProd) {
+        val doc = new SolrInputDocument()
+        doc.addField("id", "prod" + i)
+        doc.addField("brand", rand.nextString(4))
+        val childDocs = new util.ArrayList[SolrInputDocument]()
+        for (j <- 1 to Math.round(rand.nextFloat() * maxProd)) {
+          val child = new SolrInputDocument()
+          child.addField("id", "chd%d_%d".format(i, j))
+          child.addField("account", "account" + j)
+          child.addField("quantity", rand.nextInt(5000))
+          childDocs.add(child)
+        }
+        doc.addChildDocuments(childDocs)
+        server.add(doc)
+        if (i % 1000 == 0) {
+          println("commiting doc at iteration " + i)
+          server.commit()
+        }
+      }
 
       server.commit()
 
