f1da666ad5514a9319b5069a89913b42b3f098e4
Konstantin Perikov <konstantin.perikov@gmail.com>
2018-09-07 16:26:48 +0100

    update to 100 accounts

--- lucene7/src/main/scala/ExternalFileField.scala
+++ lucene7/src/main/scala/ExternalFileField.scala
@@ -21,7 +21,7 @@ object ExternalFileField {
   val rand = new Random()
 
   def main(a: Array[String]) {
-//    for (i <- 1 to maxAcc / 100) {
+//    for (i <- 1 to 99) {
 //      val out = new PrintWriter(new BufferedOutputStream(new FileOutputStream("external_account" + i)))
 //      for (i <- 1 to maxProd) {
 //        if (rand.nextInt(4) < 1) {
