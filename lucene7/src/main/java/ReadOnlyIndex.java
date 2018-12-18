7004db40f79448ec4d720287c95d526f31f34e29
Konstantin Perikov <pericov.ko@gmail.com>
2018-12-18 16:45:34 +0000

    add no lock factory

---
+++ lucene7/src/main/java/ReadOnlyIndex.java
@@ -0,0 +1,69 @@
+import com.ibm.icu.text.CollationKey;
+import com.ibm.icu.text.Collator;
+import com.ibm.icu.text.RawCollationKey;
+import com.ibm.icu.util.ULocale;
+import com.ibm.icu.util.VersionInfo;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.collation.ICUCollationDocValuesField;
+import org.apache.lucene.collation.ICUCollationKeyAnalyzer;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.*;
+import org.apache.lucene.util.BytesRef;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * @see https://stackoverflow.com/q/53438426/2663985
+ */
+public class ReadOnlyIndex {
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+//        Directory dir = FSDirectory.open(Paths.get("/tmp/test"));
+//        IndexWriterConfig iwc = new IndexWriterConfig(new StandardAnalyzer());
+//        iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+//        IndexWriter writer = new IndexWriter(dir, iwc);
+//
+//        Document doc = new Document();
+//        doc.add(new TextField("text", "bubble bloom is happening somewhere else", Field.Store.YES));
+//        writer.addDocument(doc);
+//        doc = new Document();
+//        doc.add(new TextField("text", "here we have just bloom bloom like a spring", Field.Store.YES));
+//        writer.addDocument(doc);
+//        doc = new Document();
+//        doc.add(new TextField("text", "i would be curious how bubble tea is bubble enough for me", Field.Store.YES));
+//        writer.addDocument(doc);
+//        doc = new Document();
+//        doc.add(new TextField("text", "i would be curious how bubble tea is bubble enough for me. Should I have more bubble ?", Field.Store.YES));
+//        writer.addDocument(doc);
+//        writer.close();
+
+        Directory index = FSDirectory.open(Paths.get("/tmp/test"), NoLockFactory.INSTANCE);
+        IndexReader reader = DirectoryReader.open(index);
+
+
+        IndexSearcher searcher = new IndexSearcher(reader);
+        Query query = new BooleanQuery.Builder()
+                .add(new ConstantScoreQuery(new TermQuery(new Term("text", "bloom"))), BooleanClause.Occur.SHOULD)
+                .add(new ConstantScoreQuery(new TermQuery(new Term("text", "bubble"))), BooleanClause.Occur.SHOULD)
+                .build();
+
+//        String querystr = "bubble^=1.0 bloom^=1.0c";
+//        Query query = new QueryParser("text", new StandardAnalyzer()).parse(querystr);
+
+
+        TopDocs results = searcher.search(query, 5);
+        final ScoreDoc[] scoreDocs = results.scoreDocs;
+        for (ScoreDoc scoreDoc : scoreDocs) {
+            System.out.println(scoreDoc.doc + " " + reader.document(scoreDoc.doc) + " " + scoreDoc.score);
+        }
+        System.out.println("Hits: " + results.totalHits);
+        System.out.println("Max score:" + results.getMaxScore());
+
+    }
+}
