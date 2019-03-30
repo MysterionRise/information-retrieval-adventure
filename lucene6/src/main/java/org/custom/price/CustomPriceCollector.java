2bdbc110f5030844ab439bd310a0f1f9209974a5
Konstantin Perikov <pericov.ko@gmail.com>
2019-03-30 10:29:50 +0000

    add

--- lucene6/src/main/java/org/custom/price/CustomPriceCollector.java
+++ lucene6/src/main/java/org/custom/price/CustomPriceCollector.java
@@ -2,6 +2,7 @@ package org.custom.price;
 
 import java.io.IOException;
 import java.util.*;
+
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.solr.handler.component.ResponseBuilder;
 import org.apache.solr.request.SolrRequestInfo;
@@ -9,78 +10,75 @@ import org.apache.solr.search.DelegatingCollector;
 
 public class CustomPriceCollector extends DelegatingCollector {
 
-  final Map<Integer, Integer> ids = new HashMap<>();
-  final Random r = new Random();
-  private final ResponseBuilder rb;
-  private final Map fcontext;
-  /*
-  if (postFilters != null) {
-    Collections.sort(postFilters, sortByCost);
-    for (int i = postFilters.size() - 1; i >= 0; i--) {
-      DelegatingCollector prev = pf.postFilter;
-      pf.postFilter = ((PostFilter) postFilters.get(i)).getFilterCollector(this);
-      if (prev != null) pf.postFilter.setDelegate(prev);
-    }
-  }
-   */
+    final Map<Integer, Integer> ids = new HashMap<>();
+    final Random r = new Random();
+    private final ResponseBuilder rb;
+    private final Map fcontext;
 
-  public CustomPriceCollector(ResponseBuilder rb, Map fcontext) {
-    this.rb = rb;
-    this.fcontext = SolrRequestInfo.getRequestInfo().getReq().getContext();
-    ids.clear();
-  }
-
-  @Override
-  public void collect(int doc) throws IOException {
-    // TODO GET from doc the external ID from the document like doc values
-    ids.put(doc, r.nextInt(1000));
-    //    super.collect(doc);
-  }
-
-  @Override
-  protected void doSetNextReader(LeafReaderContext context) throws IOException {
-    super.doSetNextReader(context);
-    // TODO this has been called on the end of the segment, it's time to do a batch to price system with ids
-    // we also need to evaluate through collected ids and collect them with delegate
-
-    Map<Object, Object> reqContext = SolrRequestInfo.getRequestInfo().getReq().getContext();
-
-    // TODO or check fcontext?
-
-    for (Map.Entry<Integer, Integer> e : ids.entrySet()) {
-      // TODO fix to use correct doc id
-      // TODO use reqContext which is thread local to put data about actual prices there
-      reqContext.put(e.getKey(), e.getValue());
-      super.collect(e.getKey());
+    public CustomPriceCollector(ResponseBuilder rb, Map fcontext) {
+        this.rb = rb;
+        this.fcontext = SolrRequestInfo.getRequestInfo().getReq().getContext();
+        ids.clear();
     }
 
-    rb.rsp.add("bla-bla-bla" + r.nextInt(), 100);
-    ids.clear();
-  }
-
-  @Override
-  public void finish() throws IOException {
-    // we need to do last request to price system
-    // since it will be called on the end og the last segment
-
-    // TODO we could add something to the response here
-    // TODO hack rb.rsp?
-
-    Map<Object, Object> reqContext = SolrRequestInfo.getRequestInfo().getReq().getContext();
-
-    // TODO or check fcontext?
-
-    for (Map.Entry<Integer, Integer> e : ids.entrySet()) {
-      // TODO fix to use correct doc id
-      // TODO use reqContext which is thread local to put data about actual prices there
-      reqContext.put(e.getKey(), e.getValue());
-      delegate.getLeafCollector(context).collect(e.getKey());
+    @Override
+    public void collect(int doc) throws IOException {
+        // TODO GET from doc the external ID from the document like doc values
+        ids.put(doc, r.nextInt(1000));
+        //    super.collect(doc);
     }
 
-    rb.rsp.add("bla-bla-bla" + r.nextInt(), 100);
-    rb.rsp.add("ids", ids);
-    if (delegate instanceof DelegatingCollector) {
-      ((DelegatingCollector) delegate).finish();
+    @Override
+    public boolean needsScores() {
+        return true;
     }
-  }
+
+    @Override
+    protected void doSetNextReader(LeafReaderContext context) throws IOException {
+        this.context = context;
+        this.docBase = context.docBase;
+        leafDelegate = delegate.getLeafCollector(context);
+        // TODO this has been called on the end of the segment, it's time to do a batch to price system with ids
+        // we also need to evaluate through collected ids and collect them with delegate
+
+        Map<Object, Object> reqContext = SolrRequestInfo.getRequestInfo().getReq().getContext();
+
+        // TODO or check fcontext?
+
+        for (Map.Entry<Integer, Integer> e : ids.entrySet()) {
+            // TODO fix to use correct doc id
+            // TODO use reqContext which is thread local to put data about actual prices there
+            reqContext.put(e.getKey(), e.getValue());
+            leafDelegate.collect(e.getKey());
+        }
+
+        rb.rsp.add("bla-bla-bla" + r.nextInt(), 100);
+        ids.clear();
+    }
+
+    @Override
+    public void finish() throws IOException {
+        // we need to do last request to price system
+        // since it will be called on the end og the last segment
+
+        // TODO we could add something to the response here
+        // TODO hack rb.rsp?
+
+        Map<Object, Object> reqContext = SolrRequestInfo.getRequestInfo().getReq().getContext();
+
+        // TODO or check fcontext?
+
+        for (Map.Entry<Integer, Integer> e : ids.entrySet()) {
+            // TODO fix to use correct doc id
+            // TODO use reqContext which is thread local to put data about actual prices there
+            reqContext.put(e.getKey(), e.getValue());
+            delegate.getLeafCollector(context).collect(e.getKey());
+        }
+
+        rb.rsp.add("bla-bla-bla" + r.nextInt(), 100);
+        rb.rsp.add("ids", ids);
+        if (delegate instanceof DelegatingCollector) {
+            ((DelegatingCollector) delegate).finish();
+        }
+    }
 }
--- lucene6/src/main/java/org/custom/price/EnricherCollector.java
+++ lucene6/src/main/java/org/custom/price/EnricherCollector.java
@@ -2,49 +2,45 @@ package org.custom.price;
 
 import java.io.IOException;
 import java.util.Map;
+
 import org.apache.solr.handler.component.ResponseBuilder;
 import org.apache.solr.request.SolrRequestInfo;
 import org.apache.solr.search.DelegatingCollector;
 
 public class EnricherCollector extends DelegatingCollector {
 
-  private final ResponseBuilder rb;
-  final Map fcontext;
-  /*
-  if (postFilters != null) {
-    Collections.sort(postFilters, sortByCost);
-    for (int i = postFilters.size() - 1; i >= 0; i--) {
-      DelegatingCollector prev = pf.postFilter;
-      pf.postFilter = ((PostFilter) postFilters.get(i)).getFilterCollector(this);
-      if (prev != null) pf.postFilter.setDelegate(prev);
-    }
-  }
-   */
+    private final ResponseBuilder rb;
+    final Map fcontext;
 
-  public EnricherCollector(ResponseBuilder rb, Map fcontext) {
-    this.rb = rb;
-    this.fcontext = fcontext;
-  }
-
-  @Override
-  public void collect(int doc) throws IOException {
-    Map<Object, Object> context = SolrRequestInfo.getRequestInfo().getReq().getContext();
-    if (context == null) {
-      throw new RuntimeException("context is null from request info");
+    public EnricherCollector(ResponseBuilder rb, Map fcontext) {
+        this.rb = rb;
+        this.fcontext = fcontext;
     }
-    //    int o = (int) context.get(this.docBase + doc);
-    //    if (o > 0) {
-    leafDelegate.collect(doc);
+
+    @Override
+    public boolean needsScores() {
+        return true;
+    }
+
+    @Override
+    public void collect(int doc) throws IOException {
+        Map<Object, Object> context = SolrRequestInfo.getRequestInfo().getReq().getContext();
+        if (context == null) {
+            throw new RuntimeException("context is null from request info");
+        }
+        //    int o = (int) context.get(this.docBase + doc);
+        //    if (o > 0) {
+        leafDelegate.collect(doc);
+        //    }
+        // TODO if know price we could filter document?
+    }
+
+    //  @Override
+    //  public void finish() throws IOException {
+    //    rb.rsp.add("bla", 20);
+    //
+    //    if (delegate instanceof DelegatingCollector) {
+    //      ((DelegatingCollector) delegate).finish();
     //    }
-    // TODO if know price we could filter document?
-  }
-
-  @Override
-  public void finish() throws IOException {
-    rb.rsp.add("bla", 20);
-
-    if (delegate instanceof DelegatingCollector) {
-      ((DelegatingCollector) delegate).finish();
-    }
-  }
+    //  }
 }
