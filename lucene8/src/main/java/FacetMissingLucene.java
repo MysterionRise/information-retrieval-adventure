7245c0ed83f64490c648d8f136bfa46af57faea4
Konstantin Perikov <pericov.ko@gmail.com>
2019-10-23 15:20:25 +0100

    add lucene 8 version

---
+++ lucene8/src/main/java/FacetMissingLucene.java
@@ -0,0 +1,62 @@
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @see https://stackoverflow.com/q/53374492/2663985
+ */
+public class FacetMissingLucene {
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+        Directory dir = new RAMDirectory();
+        IndexWriterConfig iwc = new IndexWriterConfig(new StandardAnalyzer());
+        iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        IndexWriter writer = new IndexWriter(dir, iwc);
+
+        Document doc = new Document();
+        doc.add(new TextField("text", "bubble bloom is happening somewhere else", Field.Store.YES));
+        doc.add(new TextField("category", "A", Field.Store.YES));
+        writer.addDocument(doc);
+        doc = new Document();
+        doc.add(new TextField("text", "bubble bloom is happening", Field.Store.YES));
+        doc.add(new TextField("category", "B", Field.Store.YES));
+        writer.addDocument(doc);
+        doc = new Document();
+        doc.add(new TextField("text", "something else", Field.Store.YES));
+        doc.add(new TextField("category", "A", Field.Store.YES));
+        writer.addDocument(doc);
+        doc = new Document();
+        doc.add(new TextField("text", "almost empty", Field.Store.YES));
+        writer.addDocument(doc);
+        writer.close();
+
+        IndexReader reader = DirectoryReader.open(dir);
+        IndexSearcher searcher = new IndexSearcher(reader);
+
+        Query q = new MatchAllDocsQuery();
+        Set<Integer> queryRes = new HashSet<>();
+        searcher.search(q, new CheckHits.SetCollector(queryRes));
+
+        Set<Integer> missingRes = new HashSet<>();
+        searcher.search(new TermRangeQuery("category", null, null, false, false), new CheckHits.SetCollector(missingRes));
+        missingRes.retainAll(queryRes);
+
+        System.out.println(missingRes.size());
+    }
+}
